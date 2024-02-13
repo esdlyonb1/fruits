0a4c894cddbd73710d66a56560782be31ee30f34
esdb1 <pierre.lacognata@mail-esd.com>
2024-02-13 11:55:20 +0100

    fruits

--- src/Controller/FruitController.php
+++ src/Controller/FruitController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Fruit;
 use App\Repository\FruitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,14 +10,20 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class FruitController extends AbstractController
 {
-    #[Route('/fruit', name: 'app_fruit')]
+    #[Route('/fruits', name: 'app_fruit')]
     public function index(FruitRepository $fruitRepository): Response
     {
-
-        $fruits  = $fruitRepository->findAll();
-
         return $this->render('fruit/index.html.twig', [
-            'fruits' => $fruits,
+            'fruits' => $fruitRepository->findAll()
+        ]);
+    }
+
+    #[Route("/fruit/{id}/{message}", name:"app_fruit_show")]
+    public function show(Fruit $fruit ):Response
+    {
+
+        return $this->render("fruit/show.html.twig", [
+            "fruit"=>$fruit,
         ]);
     }
 }
